beb2515aa6aa932aa61908a56236f1877630ac99
mikael-espinola <mikaelespinolaa@gmail.com>
2025-04-26 09:05:25 -0300

    :sparkles: feat: formulário de contato
    Implementado o formulário funcional para contato

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,17 @@
+import FormContact from "@/components/formContact/FormContact";
+import Header from "@/components/header/Header";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="layout_spacing flex flex-col items-center w-full">
+      <Header />
+      <h3 className="text-center text-lg mt-4 font-bold">
+        Contate-nos para mais informações
+      </h3>
+      <FormContact />
+    </div>
+  );
+};
+
+export default page;
